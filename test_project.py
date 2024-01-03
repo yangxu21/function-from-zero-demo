c2b62ab353c61649cea12039022de08706eee7a5
yangxu21 <155080574+yangxu21@users.noreply.github.com>
2024-01-03 21:45:46 +0000

    updating test file

--- test_project.py
+++ test_project.py
@@ -1,5 +1,13 @@
 from mylib.calc import add
+from calCLI import cli
+from click.testing import CliRunner
 
 def test_add():
     assert add(1,2) == 3
 
+
+def test_add_cmd():
+    runner = CliRunner()
+    result = runner.invoke(cli, ("add", "1", "2"))
+    assert result.exit_code == 0
+    assert "3" in result.output
